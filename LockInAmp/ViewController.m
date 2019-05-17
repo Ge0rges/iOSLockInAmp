314ff77932add47c552907dd841de0287fd24ef7
Ge0rges <ge0rges@ge0rges.com>
2019-05-17 18:56:08 -0400

    Made dot graph true. Added internal sin wave generator.

--- LockInAmp/ViewController.m
+++ LockInAmp/ViewController.m
@@ -7,19 +7,24 @@
 //
 
 #import <complex.h>
-#import <EZMicrophone.h>
+#import <EZAudio.h>
 
 #import "ViewController.h"
 
-#define Fs 44100.0
-#define Fc 440.0
-#define ALPHA 0.99
-#define rowHeight 2
+#define numberOfRows floor(tableViewHeight/rowHeight)
 
-@interface ViewController () <UITableViewDelegate, UITableViewDataSource, EZMicrophoneDelegate> {
-    UInt32 idx;
-    complex double val;
+int const rowHeight = 10;
+
+double const Fs = 44100.0;
+double const Fc = 440.0;
+double const ALPHA = 0.99;
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, EZMicrophoneDelegate, EZOutputDataSource, EZOutputDelegate> {
     NSInteger tableViewHeight;
+
+    UInt32 idx;
+
+    complex double val;
 }
 
 @property (nonatomic, strong) EZMicrophone *microphone;
@@ -27,8 +32,16 @@
 @property (strong, nonatomic) IBOutlet UITableView *dotGraphReal;
 @property (strong, nonatomic) IBOutlet UITableView *dotGraphImaginary;
 
-@property (strong, nonatomic) NSMutableArray<NSNumber *> *dotGraphRealArray;
-@property (strong, nonatomic) NSMutableArray<NSNumber *> *dotGraphImaginaryArray;
+@property (nonatomic, strong) EZOutput *output;
+
+@property (nonatomic) double amplitude;
+@property (nonatomic) double frequency;
+@property (nonatomic) double sampleRate;
+@property (nonatomic) double step;
+@property (nonatomic) double theta;
+
+@property (nonatomic, nonatomic) NSInteger realIndex;
+@property (nonatomic, nonatomic) NSInteger imaginaryIndex;
 
 @end
 
@@ -40,15 +53,6 @@
     
     tableViewHeight = self.dotGraphReal.frame.size.height;
 
-    // Setup dot graph arrays
-    self.dotGraphRealArray = [NSMutableArray new];
-    self.dotGraphImaginaryArray = [NSMutableArray new];
-    [self resetDotGraphs];
-    
-    // Setup dot graph
-    [self.dotGraphReal reloadData];
-    [self.dotGraphImaginary reloadData];
-    
     // Setup AudioSession
     AVAudioSession *session = [AVAudioSession sharedInstance];
     NSError *error;
@@ -66,6 +70,19 @@
     NSArray *inputs = [EZAudioDevice inputDevices];
     [self.microphone setDevice:[inputs lastObject]];
     [self.microphone startFetchingAudio];
+    
+    // Setup output
+    AudioStreamBasicDescription inputFormat = [EZAudioUtilities monoFloatFormatWithSampleRate:Fs];
+    self.output = [EZOutput outputWithDataSource:self inputFormat:inputFormat];
+    [self.output setDelegate:self];
+    self.frequency = Fc-1;
+    self.sampleRate = inputFormat.mSampleRate;
+    self.amplitude = 0.80;
+    
+    NSArray *outputs = [EZAudioDevice outputDevices];
+    [self.output setDevice:[outputs firstObject]];
+    [self.output startPlayback];
+
 }
 
 #pragma mark - Dot Graph Table View
@@ -74,8 +91,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSInteger rows = floor(tableViewHeight/rowHeight);
-    return rows;
+    return numberOfRows;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -88,39 +104,16 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
     }
     
-    if (tableView == self.dotGraphReal && self.dotGraphRealArray.count > indexPath.row) {
-        if ([self.dotGraphRealArray[indexPath.row] boolValue]) {
-            cell.backgroundColor = UIColor.greenColor;
-            
-        } else {
-            cell.backgroundColor = UIColor.blackColor;
-        }
-        
-    } else if (tableView == self.dotGraphImaginary && self.dotGraphImaginaryArray.count > indexPath.row) {
-        if ([self.dotGraphImaginaryArray[indexPath.row] boolValue]) {
-            cell.backgroundColor = UIColor.greenColor;
-            
-        } else {
-            cell.backgroundColor = UIColor.blackColor;
-        }
+    if (tableView == self.dotGraphReal) {
+        cell.backgroundColor = (indexPath.row == self.realIndex) ? UIColor.greenColor : UIColor.blackColor;
+    }
+    
+    if (tableView == self.dotGraphImaginary) {
+        cell.backgroundColor = (indexPath.row == self.imaginaryIndex) ? UIColor.greenColor : UIColor.blackColor;
     }
     
     return cell;
 }
-
-#pragma mark - Dot Graph Array
-- (void)resetDotGraphs {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self.dotGraphRealArray removeAllObjects];
-        [self.dotGraphImaginaryArray removeAllObjects];
-        
-        for (int i=0; i<floor(self->tableViewHeight/rowHeight); i++) {
-            [self.dotGraphRealArray insertObject:@(NO) atIndex:i];
-            [self.dotGraphImaginaryArray insertObject:@(NO) atIndex:i];
-        }
-    });
-}
-
 
 #pragma mark - Audio Processing
 -(void)microphone:(EZMicrophone *)microphone hasAudioReceived:(float **)buffer withBufferSize:(UInt32)bufferSize withNumberOfChannels:(UInt32)numberOfChannels {
@@ -131,7 +124,8 @@
     // 3. Mutate dotGraphArray with True where points should be shown on the dot graph
     // 4. Calls [self.dotGraph reloadData]; at end of function to update the dot graph
     
-    [self resetDotGraphs];
+    NSInteger oldRealIndex = self.realIndex;
+    NSInteger oldImaginaryIndex = self.imaginaryIndex;
     
     for(UInt32 i = 0; i < bufferSize; i++) {
         double arg = Fc / Fs * (idx + i) * 2 * M_PI;
@@ -143,28 +137,49 @@
     double realVal = creal(val);
     double imaginaryVal = cimag(val);
     
-    float oldMax = 0.15;
-    float oldMin = -0.15;
+    float oldMax = 0.14;
+    float oldMin = -0.14;
     float oldRange = (oldMax - oldMin);
-    float newMax = (self.dotGraphRealArray.count > 0) ? self.dotGraphRealArray.count-1 : 0;
+    float newMax = numberOfRows;
     float newMin = 0;
     float newRange = (newMax - newMin);
     
-    int realIndex = floor((((realVal - oldMin) * newRange) / oldRange) + newMin);
-    int imaginaryIndex = floor((((imaginaryVal - oldMin) * newRange) / oldRange) + newMin);
+    self.realIndex = floor((((realVal - oldMin) * newRange) / oldRange) + newMin);
+    self.imaginaryIndex = floor((((imaginaryVal - oldMin) * newRange) / oldRange) + newMin);
     
-    realIndex = MIN(realIndex, newMax);
-    realIndex = MAX(0, realIndex);
-    imaginaryIndex = MIN(imaginaryIndex, newMax);
-    imaginaryIndex = MAX(0, imaginaryIndex);
+    self.realIndex = MIN(self.realIndex, newMax);
+    self.realIndex = MAX(0, self.realIndex);
+    self.imaginaryIndex = MIN(self.imaginaryIndex, newMax);
+    self.imaginaryIndex = MAX(0, self.imaginaryIndex);
     
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self.dotGraphRealArray replaceObjectAtIndex:realIndex withObject:[NSNumber numberWithBool:YES]];
-        [self.dotGraphImaginaryArray replaceObjectAtIndex:imaginaryIndex withObject:[NSNumber numberWithBool:YES]];
+        [self.dotGraphReal reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:oldRealIndex inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
+        [self.dotGraphImaginary reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:oldImaginaryIndex inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
         
-        [self.dotGraphReal reloadData];
-        [self.dotGraphImaginary reloadData];
+        [self.dotGraphReal reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.realIndex inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
+        [self.dotGraphImaginary reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:self.imaginaryIndex inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
     });
+}
+
+#pragma mark - EZOutput DataSource & Delegate
+- (OSStatus)output:(EZOutput *)output shouldFillAudioBufferList:(AudioBufferList *)audioBufferList withNumberOfFrames:(UInt32)frames timestamp:(const AudioTimeStamp *)timestamp {
+    Float32 *buffer = (Float32 *)audioBufferList->mBuffers[0].mData;
+    
+    double thetaIncrement = 2.0 * M_PI * self.frequency / Fs;
+
+    for (UInt32 frame = 0; frame < frames; frame++) {
+        buffer[frame] = self.amplitude * sin(self.theta);
+        self.theta += thetaIncrement;
+        if (self.theta > 2.0 * M_PI) {
+            self.theta -= 2.0 * M_PI;
+        }
+    }
+    
+    return noErr;
+}
+
+- (void)output:(EZOutput *)output playedAudio:(float **)buffer withBufferSize:(UInt32)bufferSize withNumberOfChannels:(UInt32)numberOfChannels {
+    return;
 }
 
 @end
