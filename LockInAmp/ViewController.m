3fd044b105e5464ead6f6ec24c45ad3e0ce5189b
Ge0rges <ge0rges@ge0rges.com>
2019-05-20 20:10:41 -0400

    bug in ternary

--- LockInAmp/ViewController.m
+++ LockInAmp/ViewController.m
@@ -152,7 +152,7 @@ float const indicatorYMargin = 40;
     for (UInt32 frame = 0; frame < frames; frame++) {
         buffer[frame] = sin(theta);
         theta += thetaIncrement;
-        theta = (theta <= 2.0 * M_PI) ?: 0;
+        theta = (theta <= 2.0 * M_PI) ? theta : 0;
     }
     
     return noErr;
